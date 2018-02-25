8e0bddc869cefb64955ee7e56e5f4457ed0cfac5
Philip Alekseev <phil.alekseev@gmail.com>
2018-02-24 23:50:31 -0500

    Setup apollo to query data from server

--- src/screens/FeedsScreen/index.js
+++ src/screens/FeedsScreen/index.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
 import { PhotoCard } from '../../components';
+
 class FeedsScreen extends Component {
   state = { }
   render() {
+    console.log('props', this.props)
     return (
       <View>
         <PhotoCard />
@@ -12,4 +16,14 @@ class FeedsScreen extends Component {
   }
 }
 
-export default FeedsScreen;
+const getPhotos = gql`
+  query {
+    photos {
+      id
+      imageUrl
+      caption
+    }
+  }
+`
+
+export default graphql(getPhotos)(FeedsScreen);
--- src/screens/index.js
+++ src/screens/index.js
@@ -2,8 +2,13 @@ import { Navigation } from 'react-native-navigation'
 
 import FeedsScreen from './FeedsScreen'
 import ExploreScreen from './ExploreScreen'
+import WithProvider from '../components/WithProvider'
 
 export const registerScreens = () => {
-  Navigation.registerComponent('instasham_client.FeedsScreen', () => FeedsScreen)
-  Navigation.registerComponent('instasham_client.ExploreScreen', () => ExploreScreen)
+  Navigation.registerComponent('instasham_client.FeedsScreen', () =>
+    WithProvider(FeedsScreen),
+  );
+  Navigation.registerComponent('instasham_client.ExploreScreen', () => 
+    WithProvider(ExploreScreen),
+  );
 }
